0eeb44781a479d998cd1074dc0a4a18c5595cca8
Anisa Aburass <anisa.aburass@gmail.com>
2024-11-05 10:47:05 +0100

    interface erweitert

--- src/main/java/Domain/ChessPiece.java
+++ src/main/java/Domain/ChessPiece.java
@@ -1,9 +1,11 @@
 package Domain;
 
+import java.util.List;
+
 public interface ChessPiece {
 
-    boolean moveIsValid(Position moveFrom, Position moveTo, ChessPiece piece);
+    boolean moveIsValid(Position moveTo, Board board);
 
-
+    List<Position> getValidMoves(Board board);
 
 }
