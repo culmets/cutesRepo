fd44b27017857fcb22e09f1c92e93ebccc08c71e
Anisa Aburass <anisa.aburass@gmail.com>
2024-11-05 10:48:48 +0100

    created class move

---
+++ src/main/java/Domain/Move.java
@@ -0,0 +1,21 @@
+package Domain;
+
+public class Move {
+
+    private Position from;
+    private Position to;
+
+    public Move(Position from, Position to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public Position getFrom() {
+        return from;
+    }
+
+    public Position getTo() {
+        return to;
+    }
+}
+
