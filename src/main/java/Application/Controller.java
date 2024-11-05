494bb886bf644731cb9d955fd05f8569a705f9ee
Anisa Aburass <anisa.aburass@gmail.com>
2024-11-05 13:24:35 +0100

    methods added

--- src/main/java/Application/Controller.java
+++ src/main/java/Application/Controller.java
@@ -1,5 +1,15 @@
 package Application;
 
+import Domain.Board.Position;
+
 public interface Controller {
 
+    void startGame();
+    void endGame();
+    Position getMoveStart();
+    Position getMoveEnd();
+
+    void computeMove();
+    void saveGame();
+    void loadGame();
 }
---
+++ src/main/java/Domain/Game/InterfaceGame.java
@@ -0,0 +1,12 @@
+package Domain.Game;
+
+public interface InterfaceGame {
+
+    void startGame();
+
+    boolean isGameOver();
+
+    String getWinner();
+
+
+}
