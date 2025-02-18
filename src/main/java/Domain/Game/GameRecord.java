f80d46b763fb39c79c1bd1d6d16872b2542ec80a
Anisa Aburass <anisa.aburass@gmail.com>
2025-02-18 14:22:27 +0100

    added classes to document game

---
+++ src/main/java/Domain/Game/GameRecord.java
@@ -0,0 +1,53 @@
+package Domain.Game;
+
+import Domain.Board.Move;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class GameRecord {
+
+    private String whitePlayer;
+    private String blackPlayer;
+    private String winner; // "white", "black" oder "draw"
+    private List<Move> moveHistory;
+    private LocalDateTime gameDate;
+
+    public GameRecord(String whitePlayer, String blackPlayer, String winner, List<Move> moveHistory, LocalDateTime gameDate) {
+        this.whitePlayer = whitePlayer;
+        this.blackPlayer = blackPlayer;
+        this.winner = winner;
+        this.moveHistory = moveHistory;
+        this.gameDate = gameDate;
+    }
+
+    public String getBlackPlayer() {
+        return blackPlayer;
+    }
+
+    public LocalDateTime getGameDate() {
+        return gameDate;
+    }
+
+    public List<Move> getMoveHistory() {
+        return moveHistory;
+    }
+
+    public String getWhitePlayer() {
+        return whitePlayer;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    @Override
+    public String toString() {
+        return "White: " + whitePlayer + "\n" +
+                "Black: " + blackPlayer + "\n" +
+                "Winner: " + winner + "\n" +
+                "Date: " + gameDate + "\n" +
+                "Moves:\n" + moveHistory.toString();
+    }
+
+}
---
+++ src/main/java/Domain/Game/MoveHistory.java
@@ -0,0 +1,38 @@
+package Domain.Game;
+
+import Domain.Board.Move;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MoveHistory {
+
+    private final List<Move> moves;
+
+    public MoveHistory() {
+        moves = new ArrayList<>();
+    }
+
+    public void addMove(Move move) {
+        moves.add(move);
+    }
+
+    public List<Move> getDemMoves() {
+        return Collections.unmodifiableList(moves);
+    }
+
+    public int size() {
+        return moves.size();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Move move : moves) {
+            sb.append(move.toString()).append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
+
+}
