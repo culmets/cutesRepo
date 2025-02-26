c676287f34d79ea0815493de4717fd8e782afe79
Anisa Aburass <anisa.aburass@gmail.com>
2025-02-26 13:41:30 +0100

    class converted to record

--- src/main/java/Domain/Game/GameRecord.java
+++ src/main/java/Domain/Game/GameRecord.java
@@ -3,41 +3,11 @@ package Domain.Game;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class GameRecord {
-
-    private String whitePlayer;
-    private String blackPlayer;
-    private String winner; // "white", "black" oder "draw"
-    private List<Move> moveHistory;
-    private LocalDateTime gameDate;
-
-    public GameRecord(String whitePlayer, String blackPlayer, String winner, List<Move> moveHistory, LocalDateTime gameDate) {
-        this.whitePlayer = whitePlayer;
-        this.blackPlayer = blackPlayer;
-        this.winner = winner;
-        this.moveHistory = moveHistory;
-        this.gameDate = gameDate;
-    }
-
-    public String getBlackPlayer() {
-        return blackPlayer;
-    }
-
-    public LocalDateTime getGameDate() {
-        return gameDate;
-    }
-
-    public List<Move> getMoveHistory() {
-        return moveHistory;
-    }
-
-    public String getWhitePlayer() {
-        return whitePlayer;
-    }
-
-    public String getWinner() {
-        return winner;
-    }
+/**
+ * @param winner "white", "black" oder "draw"
+ */
+public record GameRecord(String whitePlayer, String blackPlayer, String winner, List<Move> moveHistory,
+                         LocalDateTime gameDate) {
 
     @Override
     public String toString() {
